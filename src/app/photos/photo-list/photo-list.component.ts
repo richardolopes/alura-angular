30a94d399956d347d8048c8901924da66745886a
unknown <richardxlopes@gmail.com>
2020-05-31 02:34:43 -0300

    PipeTransform, OnDestroy, paginacao

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-  photos: Photo[];
+export class PhotoListComponent implements OnInit, OnDestroy {
+  photos: Photo[] = [];
+  filter = '';
+  debounce = new Subject<string>();
+  hasMore = true;
+  currentPage = 1;
+  user = '';
 
-  constructor(private service: PhotoService, private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) { }
 
   ngOnInit(): void {
-    const user = this.activatedRoute.snapshot.params.user;
+    this.user = this.activatedRoute.snapshot.params.user;
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
 
-    this.service
-      .listFromUser(user)
-      .subscribe(photos => this.photos = photos);
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load(): void {
+    this.photoService.listFromUser(this.user, ++this.currentPage).subscribe(
+      photos => {
+        this.photos = this.photos.concat(photos);
+
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      }
+    );
   }
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Photo } from './photo';
@@ -11,7 +11,8 @@ export class PhotoService {
 
     constructor(private http: HttpClient) { }
 
-    listFromUser(username: string): Observable<Photo[]> {
-        return this.http.get<Photo[]>(`${this.endpoint + username}/photos`);
+    listFromUser(username: string, page: number): Observable<Photo[]> {
+        const params = new HttpParams().append('page', page.toString());
+        return this.http.get<Photo[]>(`${this.endpoint + username}/photos`, { params });
     }
 }
