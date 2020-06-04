c2226553785aacbc6f65b229e97ba39713ef658c
unknown <richardxlopes@gmail.com>
2020-06-03 22:25:19 -0300

    armazenamento do token, header de resposta

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const URL = 'http://localhost:3000/';
 
@@ -9,12 +11,16 @@ const URL = 'http://localhost:3000/';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private token: TokenService) { }
 
   authenticate(user: string, password: string): Observable<any> {
     return this.http.post(URL + 'user/login', {
       userName: user,
       password
-    })
+    }, { observe: 'response' })
+      .pipe(tap(res => {
+        const token = res.headers.get('x-access-token');
+        this.token.setToken(token);
+      }))
   }
 }
